4f14186ecf7adc9ff9a4b8368c3f2c0d6050d73f
kush-raj <rajkushwaha.work@gmail.com>
2026-04-30 17:52:20 +0530

    port fixing

--- server/server.js
+++ server/server.js
@@ -74,7 +74,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 // if (process.env.NODE_ENV !== 'production') {
 //   app.listen(PORT, () => {
